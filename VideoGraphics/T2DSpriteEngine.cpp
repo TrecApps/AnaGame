38756a3018a1059b04f85905f0488850c7052152
John Jacko <jljacko@outlook.com>
2020-09-03 16:56:55 -0600

    Added Attributes to Sprite Engine

--- VideoGraphics/T2DSpriteEngine.cpp
+++ VideoGraphics/T2DSpriteEngine.cpp
@@ -1,1 +1,44 @@
 #include "T2DSpriteEngine.h"
+
+T2DSprite::T2DSprite(TDataArray<TrecSubPointer<TBrush, TBitmapBrush>>& brushes, UINT index)
+{
+}
+
+void T2DSprite::SetTiltDegrees(float degrees)
+{
+}
+
+float T2DSprite::GetTiltDegrees()
+{
+    return 0.0f;
+}
+
+void T2DSprite::OnDraw(D2D1_RECT_F& loc)
+{
+}
+
+UINT T2DSprite::GetIndex()
+{
+    return 0;
+}
+
+T2DSpriteEngine::T2DSpriteEngine(TrecPointer<DrawingBoard>)
+{
+}
+
+T2DSpriteEngine::~T2DSpriteEngine()
+{
+}
+
+UINT T2DSpriteEngine::Initialize(TDataArray<TFileShell>& files)
+{
+    return 0;
+}
+
+void T2DSpriteEngine::SetCenter(UINT row, UINT column)
+{
+}
+
+void T2DSpriteEngine::Draw(D2D1_RECT_F& space)
+{
+}
--- VideoGraphics/T2DSpriteEngine.h
+++ VideoGraphics/T2DSpriteEngine.h
@@ -1,11 +1,53 @@
 #pragma once
 #include <TObject.h>
+#include "DrawingBoard.h"
 
+class T2DSprite
+{
+public:
+    T2DSprite(TDataArray<TrecSubPointer<TBrush, TBitmapBrush>>& brushes, UINT index);
 
+    void SetTiltDegrees(float degrees);
+    float GetTiltDegrees();
 
+    void OnDraw(D2D1_RECT_F& loc);
+
+    UINT GetIndex();
+
+protected:
+    float tilt;
+    UINT index;
+    TrecSubPointer<TBrush, TBitmapBrush> brush;
+};
+
+/**
+ * Class: T2DSpriteEngine
+ * Purpose: Hosts a simple Sprite collection
+ */
 class T2DSpriteEngine :
     public TObject
 {
-    
+public: 
+    T2DSpriteEngine(TrecPointer<DrawingBoard>);
+
+    virtual ~T2DSpriteEngine();
+
+    UINT Initialize(TDataArray<TFileShell>& files);
+
+    void SetCenter(UINT row, UINT column);
+
+    void Draw(D2D1_RECT_F& space);
+
+protected:
+
+    TrecPointer<DrawingBoard> board;
+    D2D1_RECT_F location;
+
+    UINT rows, columns;
+    UINT centerRow, centerColumn;
+
+    UINT wholeSprites;
+
+    TDataArray<T2DSprite> sprites;
 };
 
