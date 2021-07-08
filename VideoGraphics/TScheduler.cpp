2b4e7d4293dca29d63745336f0b78e775b874b65
John Jacko <jljacko@outlook.com>
2021-07-08 15:59:18 -0500

    Finished coding the scheduler

--- VideoGraphics/TScheduler.cpp
+++ VideoGraphics/TScheduler.cpp
@@ -3,6 +3,12 @@
 #include <mfapi.h>
 #include <Mferror.h>
 
+/**
+ * Method: TScheduler::TScheduler
+ * Purpose: Constructor
+ * Parameters:TrecComPointer<IMFStreamSink> s - the stream sink using this scheduler
+ * Returns: New Scheduler Object
+ */
 TScheduler::TScheduler(TrecComPointer<IMFStreamSink> s):
 	callBack(this, &TScheduler::OnTimer)
 {
@@ -11,40 +17,86 @@ TScheduler::TScheduler(TrecComPointer<IMFStreamSink> s):
 	this->timerKey = 0;
 }
 
+/**
+ * Method: TScheduler::~TScheduler
+ * Purpose: Destructor
+ * Parameters: void
+ * Returns: void
+ */
 TScheduler::~TScheduler()
 {
 }
 
+/**
+ * Method: TScheduler::SetFrameRate
+ * Purpose: Sets the new Frame-rate
+ * Parameters: const MFRatio& fps -  the frame rarte to work with
+ * Returns: void
+ */
 void TScheduler::SetFrameRate(const MFRatio& fps)
 {
+	// Set basic value
 	UINT64 timeFrame = 0;
 
+	// Perform calculation
 	MFFrameRateToAverageTimePerFrame(fps.Numerator, fps.Denominator, &timeFrame);
+	// Convert the value result
 	perFrameInterval = (MFTIME)timeFrame;
+	// Gets used often, save at least one calculation
 	perFrameQuarter = perFrameInterval / 4;
 }
 
+/**
+ * Method: TScheduler::SetClockRate
+ * Purpose: Sets the clock rate
+ * Parameters: float fRate -  the new rate to use
+ * Returns: HRESULT - S_OK
+ */
 HRESULT TScheduler::SetClockRate(float fRate)
 {
 	frameRate = fRate;
 	return S_OK;
 }
 
+/**
+ * Method: TScheduler::LastSampleTime
+ * Purpose: Reports the time of the last sample
+ * Parameters: void
+ * Returns: LONGLONG - the last sample processed and the time it was
+ *
+ * Note: Value not set
+ */
 const LONGLONG& TScheduler::LastSampleTime(void) const
 {
 	return lastSampleTime;
 }
 
+/**
+ * Method: TScheduler::FrameDuration
+ * Purpose: Reports the frame duration
+ * Parameters: void
+ * Returns:LONGLONG - the frame duration
+ *
+ * Note: Value not set
+ */
 const LONGLONG& TScheduler::FrameDuration(void) const
 {
 	return perFrameInterval;
 }
 
+/**
+ * Method: TScheduler::StartScheduler
+ * Purpose: Sets up a New clock
+ * Parameters:IMFClock* pClock - the clock to work with
+ * Returns: HRESULT - should be S_OK
+ */
 HRESULT TScheduler::StartScheduler(IMFClock* pClock)
 {
+	// If we currently have a clock, prepare to get rid of it
 	if (clock)
 		clock->Release();
 	clock = pClock;
+	// If we have a new clock, update reference counter
 	if (clock)
 		clock->AddRef();
 
@@ -54,21 +106,38 @@ HRESULT TScheduler::StartScheduler(IMFClock* pClock)
 	return waitTimer ? S_OK : HRESULT_FROM_WIN32(GetLastError());
 }
 
+/**
+ * Method: TScheduler::StopScheduler
+ * Purpose: Closes the timer set up and releases the current clock
+ * Parameters: void
+ * Returns: HRESULT - S_OK
+ */
 HRESULT TScheduler::StopScheduler(void)
 {
+	// If we have a timer, close it and nullify it
 	if (waitTimer)
 	{
 		CloseHandle(waitTimer);
 		waitTimer = nullptr;
 	}
-
+	// Remove any sample we may have
 	samples.Flush();
 	timeEndPeriod(1);
+
+	// Remove the exising clock
 	if (clock)
 		clock->Release();
+	clock = nullptr;
 	return S_OK;
 }
 
+/**
+ * Method: TScheduler::ScheduleSample
+ * Purpose: Schedules a sample for presentation
+ * Parameters: TSampleTexture* pSample - the sample to process
+ *              bool bPesentNow - whether to present the frame upon this call
+ * Returns: HRESULT - Should be S_OK
+ */
 HRESULT TScheduler::ScheduleSample(TSampleTexture* pSample, bool now)
 {
 	if (!streamer.Get())
@@ -76,6 +145,7 @@ HRESULT TScheduler::ScheduleSample(TSampleTexture* pSample, bool now)
 	HRESULT ret = S_OK;
 	ThreadLock();
 
+	// If told to present now (or we don't have a clock), present the current frame
 	if (now || !clock)
 	{
 		dynamic_cast<TStreamSink*>(streamer.Get())->PresentFrame();
@@ -91,16 +161,26 @@ HRESULT TScheduler::ScheduleSample(TSampleTexture* pSample, bool now)
 	return ret;
 }
 
+
+/**
+ * Method: TScheduler::ProcessSamplesInQueue
+ * Purpose: Processes the samples currently in the queue
+ * Parameters: LONG* plNextSleep - how long the thread should sleep
+ * Returns: HRESULT - should be S_OK
+ */
 HRESULT TScheduler::ProcessSamplesInQueue(LONG* plNextSleep)
 {
+	if (!plNextSleep) return E_POINTER;
 	HRESULT ret = S_OK;
 	LONG wait = 0;
 	ThreadLock();
 	TSampleTexture* sample = nullptr;
 	while (samples.Dequeue(sample) && sample)
 	{
+		// Process the sample from the queue
 		ret = ProcessSample(sample, &wait);
 		sample->Release();
+		// If we need to sleep, go ahead and sleep
 		if (FAILED(ret) || wait > 0)
 			break;
 	}
@@ -112,6 +192,13 @@ HRESULT TScheduler::ProcessSamplesInQueue(LONG* plNextSleep)
 	return ret;
 }
 
+/**
+ * Method: TScheduler::ProcessSample
+ * Purpose: Checks the timing of the sample relative to the current time and determines if to present of store
+ * Parameters: TSampleTexture* pSample - the sample to process
+ *              LONG* plNextSleep - how long the thread should sleep
+ * Returns: HRESULT - should be S_OK
+ */
 HRESULT TScheduler::ProcessSample(TSampleTexture* pSample, LONG* plNextSleep)
 {
 	ThreadLock();
@@ -126,24 +213,31 @@ HRESULT TScheduler::ProcessSample(TSampleTexture* pSample, LONG* plNextSleep)
 		MFTIME   systemTime = 0;
 		LONGLONG delta = 0;
 
+		// Get the IMFSample
 		IMFSample* samp = nullptr;
 		pSample->QueryInterface(__uuidof(IMFSample), (void**)&samp);
 
+		// Get the sample time
 		ret = samp->GetSampleTime(&presentTime);
 		if (SUCCEEDED(ret))
 		{
+			// Get info on the current time
 			ret =clock->GetCorrelatedTime(0, &currentTime, &systemTime);
 		}
 		if (SUCCEEDED(ret))
 		{
-			delta = presentTime = currentTime;
+			// Figure out time between now and when sample should be displayed
+			delta = presentTime - currentTime;
+			// In case video is playing backwards
 			if (frameRate < 0)
 				delta = -delta;
 
+			// Check of negative
 			if (delta < -this->perFrameQuarter)
 				doPresent = true;
 			else if (delta > (3 * perFrameQuarter))
 			{
+				// Prepare to hold off on presentig the sample
 				nextSleep = (delta - (3 * perFrameQuarter)) / (10000.0 * frameRate);
 				doPresent = false;
 			}
@@ -151,17 +245,27 @@ HRESULT TScheduler::ProcessSample(TSampleTexture* pSample, LONG* plNextSleep)
 
 	}
 
+	// Check to see if sample needs to be presented now
 	if (doPresent)
 		ret = dynamic_cast<TStreamSink*>(streamer.Get())->PresentFrame();
 	else
 	{
+		// Push back to queue
 		pSample->AddRef();
 		samples.Push(pSample);
 	}
+
+	*plNextSleep = nextSleep;
 	ThreadRelease();
 	return ret;
 }
 
+/**
+ * Method: TScheduler::Flush
+ * Purpose: Undoes the timer and removes all frames
+ * Parameters: coid
+ * Returns: HRESULT - S_OK
+ */
 HRESULT TScheduler::Flush(void)
 {
 	ThreadLock();
@@ -177,23 +281,38 @@ HRESULT TScheduler::Flush(void)
 	return S_OK;
 }
 
+/**
+ * Method: TScheduler::GetSampleCount
+ * Purpose: Reports the number of samples currently held by the scheduler
+ * Parameters: void
+ * Returns: UINT - number of samples currently being held
+ */
 UINT TScheduler::GetSampleCount()
 {
 	return samples.GetSize();
 }
 
+/**
+ * Method: TScheduler::StartProcessSample
+ * Purpose: Begins the Processing queue, taking into account when the frame should be presented
+ * Parameters: void
+ * Returns: HRESULT - should be S_OK
+ */
 HRESULT TScheduler::StartProcessSample()
 {
 	LONG wait = INFINITE;
 	bool exitThread = false;
 	IMFAsyncResult* asyncRes = nullptr;
 
+	// Start Processing Samples
 	HRESULT ret = ProcessSamplesInQueue(&wait);
 
 	if (SUCCEEDED(ret))
 	{
+		// If we need to sleep (because the sample is not scheduled to proceed
 		if (INFINITE != wait && 0 < wait)
 		{
+			// Set a timer for when to present
 			LARGE_INTEGER llDueTime;
 			llDueTime.QuadPart = -10000 * wait;
 			if (SetWaitableTimer(waitTimer, &llDueTime, 0, NULL, NULL, FALSE) == 0)
@@ -203,7 +322,7 @@ HRESULT TScheduler::StartProcessSample()
 
 			if (SUCCEEDED(ret))
 			{
-				// queue a waititem to wait for timer completion
+				// queue a wait item to wait for timer completion
 				ret = MFCreateAsyncResult(nullptr, &callBack, nullptr, &asyncRes);
 				if (SUCCEEDED(ret))
 				{
@@ -218,7 +337,13 @@ HRESULT TScheduler::StartProcessSample()
 	return ret;
 }
 
-HRESULT TScheduler::OnTimer(IMFAsyncResult* pResult)
+/**
+ * Method: TScheduler::OnTimer
+ * Purpose: Callback for running in a seperate thread
+ * Parameters: IMFAsyncResult* pResult - standard parameter for Media Foundation callbacks
+ * Returns: HRESULT - Result of processing the frames
+ */
+HRESULT TScheduler::OnTimer(__RPC__in_opt IMFAsyncResult* pResult)
 {
 	ThreadLock();
 
--- VideoGraphics/TScheduler.h
+++ VideoGraphics/TScheduler.h
@@ -16,23 +16,107 @@ class TScheduler :
 {
 public:
 
+    /**
+     * Method: TScheduler::TScheduler
+     * Purpose: Constructor
+     * Parameters:TrecComPointer<IMFStreamSink> s - the stream sink using this scheduler
+     * Returns: New Scheduler Object
+     */
     TScheduler(TrecComPointer<IMFStreamSink> s);
+    /**
+     * Method: TScheduler::~TScheduler
+     * Purpose: Destructor
+     * Parameters: void
+     * Returns: void
+     */
     virtual ~TScheduler();
 
+    /**
+     * Method: TScheduler::SetFrameRate
+     * Purpose: Sets the new Frame-rate
+     * Parameters: const MFRatio& fps -  the frame rarte to work with
+     * Returns: void
+     */
     void SetFrameRate(const MFRatio& fps);
-    HRESULT SetClockRate(float fRate);// { m_fRate = fRate; }
+    /**
+     * Method: TScheduler::SetClockRate
+     * Purpose: Sets the clock rate
+     * Parameters: float fRate -  the new rate to use
+     * Returns: HRESULT - S_OK
+     */
+    HRESULT SetClockRate(float fRate);
 
-    const LONGLONG& LastSampleTime(void) const;// { return m_LastSampleTime; }
-    const LONGLONG& FrameDuration(void) const;// { return m_PerFrameInterval; }
+    /**
+     * Method: TScheduler::LastSampleTime
+     * Purpose:Reports the time of the last sample
+     * Parameters: 
+     * Returns: LONGLONG - the last sample processed and the time it was
+     * 
+     * Note: Value not set
+     */
+    const LONGLONG& LastSampleTime(void) const;
+    /**
+     * Method: TScheduler::FrameDuration
+     * Purpose: Reports the frame duration
+     * Parameters: void
+     * Returns:LONGLONG - the frame duration
+     * 
+     * Note: Value not set
+     */
+    const LONGLONG& FrameDuration(void) const;
 
+    /**
+     * Method: TScheduler::StartScheduler
+     * Purpose: Sets up a New clock
+     * Parameters:IMFClock* pClock - the clock to work with
+     * Returns: HRESULT - should be S_OK
+     */
     HRESULT StartScheduler(IMFClock* pClock);
+    /**
+     * Method: TScheduler::StopScheduler
+     * Purpose: Closes the timer set up and releases the current clock
+     * Parameters: void
+     * Returns: HRESULT - S_OK
+     */
     HRESULT StopScheduler(void);
 
+    /**
+     * Method: TScheduler::ScheduleSample
+     * Purpose: Schedules a sample for presentation
+     * Parameters: TSampleTexture* pSample - the sample to process 
+     *              bool bPesentNow - whether to present the frame upon this call
+     * Returns: HRESULT - Should be S_OK
+     */
     HRESULT ScheduleSample(TSampleTexture* pSample, bool bPesentNow);
+    /**
+     * Method: TScheduler::ProcessSamplesInQueue
+     * Purpose: Processes the samples currently in the queue
+     * Parameters: LONG* plNextSleep - how long the thread should sleep
+     * Returns: HRESULT - should be S_OK
+     */
     HRESULT ProcessSamplesInQueue(LONG* plNextSleep);
+    /**
+     * Method: TScheduler::ProcessSample
+     * Purpose: Checks the timing of the sample relative to the current time and determines if to present of store
+     * Parameters: TSampleTexture* pSample - the sample to process
+     *              LONG* plNextSleep - how long the thread should sleep
+     * Returns: HRESULT - should be S_OK
+     */
     HRESULT ProcessSample(TSampleTexture* pSample, LONG* plNextSleep);
+    /**
+     * Method: TScheduler::Flush
+     * Purpose: Undoes the timer and removes all frames
+     * Parameters: coid
+     * Returns: HRESULT - S_OK
+     */
     HRESULT Flush(void);
 
+    /**
+     * Method: TScheduler::GetSampleCount
+     * Purpose: Reports the number of samples currently held by the scheduler
+     * Parameters: void
+     * Returns: UINT - number of samples currently being held
+     */
     UINT GetSampleCount();
 
     //ULONG AddRef();
@@ -40,7 +124,19 @@ public:
     //HRESULT QueryInterface(,void** ppv);
 
 protected:
+    /**
+     * Method: TScheduler::StartProcessSample
+     * Purpose: Begins the Processing queue, taking into account when the frame should be presented
+     * Parameters: void
+     * Returns: HRESULT - should be S_OK
+     */
     HRESULT StartProcessSample();
+    /**
+     * Method: TScheduler::OnTimer
+     * Purpose: Callback for running in a seperate thread
+     * Parameters: IMFAsyncResult* pResult - standard parameter for Media Foundation callbacks
+     * Returns: HRESULT - Result of processing the frames
+     */
     HRESULT OnTimer(__RPC__in_opt IMFAsyncResult* pResult);
     TComCallback<TScheduler> callBack;                 // Callback mechanism
 
