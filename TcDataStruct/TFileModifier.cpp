fb2e2379f776595730bd4ae6522c624dc6ab5825
John Jacko <jljacko@outlook.com>
2021-10-04 16:48:57 -0500

    Added new Base class for modifying File Contents

---
+++ TcDataStruct/TFileModifier.cpp
@@ -0,0 +1,19 @@
+#include "TFileModifier.h"
+#include <TFile.h>
+
+TFileModifier::TFileModifier(TrecPointer<TFileShell> inputFile, const TString& outputFile)
+{
+    this->inputFile = inputFile;
+    this->outputFile.Set(outputFile);
+    if (!inputFile.Get())
+        throw L"Invalid Input File";
+    TFile file(outputFile, TFile::t_file_open_always);
+    if (!file.IsOpen())
+        throw L"Invalid Output File Path";
+    file.Close();
+}
+
+TString TFileModifier::Modify()
+{
+    return Modify(L"default");
+}
---
+++ TcDataStruct/TFileModifier.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <TObject.h>
+#include <TFileShell.h>
+
+/**
+ * Class: TFileHandler
+ * Purpose: Allows Files to be Modified before they are actually used (Can be used to clean up)
+ */
+class TFileModifier :
+    public TObject
+{
+public:
+    /**
+     * Method: TFileModifier::TFileModifier
+     * Purpose: Constructor
+     * Parameters: TrecPointer<TFileShell> inputFile - input file
+     *              const TString& output - output file (in string form as the output file might not exist yet)
+     * Returns: new File Modifier
+     */
+    TFileModifier(TrecPointer<TFileShell> inputFile, const TString& outputFile);
+
+    /**
+     * Method: TFileModifier::Modify
+     * Purpose: Calls the Default operation
+     * Parameters: const TString& details - information that might influence the process of File Modification
+     * Returns: TString - error information (empty string means no error)
+     * 
+     * Attributes: abstract
+     */
+    virtual TString Modify(const TString& details) = 0;
+
+    /**
+     * Method: TFileModifier::Modify
+     * Purpose: Calls the Default operation
+     * Parameters: void
+     * Returns: TString - error information (empty string means no error)
+     */
+    TString Modify();
+
+protected:
+
+    /**
+     * Input File
+     */
+    TrecPointer<TFileShell> inputFile;
+
+    /**
+     * Output File Path
+     */
+    TString outputFile;
+};
