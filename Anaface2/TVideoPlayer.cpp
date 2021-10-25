d5798cc9f059b69aa0a6d4c8c727a82fa3c47819
John Jacko <jljacko@outlook.com>
2021-10-25 10:49:29 -0500

    Added TVideoPlayer to Anaface 2

---
+++ Anaface2/TVideoPlayer.cpp
@@ -0,0 +1,100 @@
+#include "TVideoPlayer.h"
+
+TVideoPlayer::TVideoPlayer(TrecPointer<DrawingBoard> rt, TrecPointer<TArray<styleTable>> ta): TControl(rt, ta)
+{
+	HRESULT res;
+	player = TPlayer::CreateInstance(res, rt);
+	assert(SUCCEEDED(res));
+}
+
+TVideoPlayer::~TVideoPlayer()
+{
+}
+
+bool TVideoPlayer::setVideo(const TString& str)
+{
+	TObjectLocker lock(&thread);
+	return player.Get() && SUCCEEDED(player->OpenURL(str, this->windowEngine));
+
+}
+
+bool TVideoPlayer::Play()
+{
+	TObjectLocker lock(&thread);
+	return player.Get() && SUCCEEDED(player->Play());
+}
+
+bool TVideoPlayer::Pause()
+{
+	TObjectLocker lock(&thread);
+	return player.Get() && SUCCEEDED(player->Pause());
+}
+
+bool TVideoPlayer::Stop()
+{
+	TObjectLocker lock(&thread);
+	return player.Get() && SUCCEEDED(player->Stop());
+}
+
+bool TVideoPlayer::Speed(float)
+{
+	TObjectLocker lock(&thread);
+	return false;
+}
+
+bool TVideoPlayer::onCreate(const D2D1_RECT_F& loc, TrecPointer<TWindowEngine> d3d)
+{
+	TObjectLocker lock(&thread);
+	if (!player.Get())
+	{
+		return false;
+	}
+	windowEngine = d3d;
+
+	// Don't want to override non-direct 2D Content
+
+	bool returnable = TControl::onCreate(loc, d3d);
+	TString valpoint;;
+	if (attributes.retrieveEntry(L"|MediaSource", valpoint))
+	{
+		if (!setVideo(valpoint))
+			returnable = false;
+	}
+	RECT rr;
+	rr.top = area.top;
+	rr.left = area.left;
+	rr.right = area.right;
+	rr.bottom = area.bottom;
+
+	player->ResizeVideo(rr);
+	return returnable;
+}
+
+void TVideoPlayer::Draw(TrecPointer<TVariable> object)
+{
+	TObjectLocker lock(&thread);
+	TControl::Draw(object);
+	if (player.Get())
+		player->Repaint();
+}
+
+void TVideoPlayer::OnResize(D2D1_RECT_F& newLoc, UINT nFlags, TDataArray<EventID_Cred>& eventAr, TDataArray<EventArgs>& args)
+{
+	TObjectLocker lock(&thread);
+	TControl::OnResize(newLoc, nFlags, eventAr, args);
+	if (player.Get())
+	{
+		RECT rr;
+		rr.top = area.top;
+		rr.left = area.left;
+		rr.right = area.right;
+		rr.bottom = area.bottom;
+		player->ResizeVideo(rr);
+	}
+}
+
+TrecComPointer<TPlayer> TVideoPlayer::GetPlayer()
+{
+	TObjectLocker lock(&thread);
+	return player;
+}
---
+++ Anaface2/TVideoPlayer.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "TControl.h"
+#include <TPlayer.h>
+class TVideoPlayer :
+    public TControl
+{
+public:
+    TVideoPlayer(TrecPointer<DrawingBoard>, TrecPointer<TArray<styleTable>> ta);
+    ~TVideoPlayer();
+	bool setVideo(const TString& str);
+    bool Play();
+    bool Pause();
+    bool Stop();
+    bool Speed(float);
+	/**
+	 * Method: TControl::onCreate
+	 * Purpose: Allows the Control To contstruct itself based off of the location it has and the
+	 *		screen space it is given
+	 * Parameters: const D2D1_RECT_F& loc- the location this control is bound by
+	 * Returns: bool - success
+	 * Note: You Must call this on the Root Control before any control can be drawn on sreen
+	 *
+	 * Attributes: virtual
+	 */
+	virtual bool onCreate(const D2D1_RECT_F& loc, TrecPointer<TWindowEngine> d3d);
+
+	/**
+	 * Method: TControl::Draw
+	 * Purpose: Draws the Page to the Window
+	 * Parameters: TrecPointer<TVariable> object - Memory Safe means of enabling Data-Binding, if the Page has to tailor it's drawing to data provided by this parameter
+	 * Returns: void
+	 *
+	 * Attributes: override
+	 */
+	virtual void Draw(TrecPointer<TVariable> object) override;
+
+	/**
+	 * Method: TControl::OnResize
+	 * Purpose: Resizes the Page
+	 * Parameters: D2D1_RECT_F& newLoc - the new regoin of the Page
+	 *				UINT nFlags - flags associated with the move
+	 *				TrecPointer<TWindowEngine> - the 3D Engine to work with
+	 *				TDataArray<EventID_Cred>& eventAr - list of events
+	 * Returns: void
+	 *
+	 * Note: May be Deprecated soon once the MiniHandler is removed from the library
+	 *
+	 * Attributes: message; override
+	 */
+	ag_msg virtual void OnResize(D2D1_RECT_F& newLoc, UINT nFlags, TDataArray<EventID_Cred>& eventAr, TDataArray<EventArgs>&) override;
+
+
+    TrecComPointer<TPlayer> GetPlayer();
+private:
+    TrecComPointer<TPlayer> player;
+    TrecPointer<TWindowEngine> windowEngine;
+};
+
