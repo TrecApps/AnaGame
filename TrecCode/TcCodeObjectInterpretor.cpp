69dc9b9140cc1f774c5cc6c5bfaffa6b402070f6
John Jacko <jljacko@outlook.com>
2021-11-25 21:27:18 -0600

    Established Methods for Generic Object interpretor

--- TrecCode/TcCodeObjectInterpretor.cpp
+++ TrecCode/TcCodeObjectInterpretor.cpp
@@ -1,1 +1,39 @@
 #include "TcCodeObjectInterpretor.h"
+
+TcCodeObjectInterpretor::TcCodeObjectInterpretor(TrecPointer<TVariable> language, TrecPointer<TEnvironment> env): TcTypeInterpretor(TrecSubPointer<TVariable, TcInterpretor>(), env)
+{
+}
+
+TcCodeObjectInterpretor::TcCodeObjectInterpretor(TrecSubPointer<TVariable, TcInterpretor> language, TrecPointer<TEnvironment> env) :TcTypeInterpretor(language, env)
+{
+}
+
+COMPILE_TYPE TcCodeObjectInterpretor::CanCompile()
+{
+	return COMPILE_TYPE();
+}
+
+void TcCodeObjectInterpretor::Compile(COMPILE_TYPE type, ReturnObject& ret)
+{
+}
+
+void TcCodeObjectInterpretor::SetFile(TrecPointer<TFileShell> codeFile, ReturnObject& ret, bool isFirst)
+{
+}
+
+ReturnObject TcCodeObjectInterpretor::Run()
+{
+	return ReturnObject();
+}
+
+void TcCodeObjectInterpretor::SetIntialVariables(TDataArray<TrecPointer<TVariable>>& params)
+{
+}
+
+void TcCodeObjectInterpretor::PreProcess(ReturnObject& ret)
+{
+}
+
+void TcCodeObjectInterpretor::ProcessIndividualStatement(const TString& statement, ReturnObject& ret)
+{
+}
