d074df43f2a5d1febff742b97126dfae6a0b44ed
John Jacko <jljacko@outlook.com>
2022-06-25 17:09:34 -0500

    Began CBase compiler

---
+++ TrecCode/CBaseCompiler.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <AgCompiler.h>
+class CBaseCompiler : public AgCompiler
+{
+private:
+	CBaseCompiler(TrecPointer<TFileShell> file);
+	TrecPointer<TFileShell> file;
+
+	TDataArray<TcPreStatement> preStatements;
+	TDataArray<TrecPointer<TcBaseStatement>> baseStatements;
+public:
+
+	/**
+	 * Class: AgCompiler::Builder
+	 * Purpose: Interface for Building a Compiler
+	 */
+	class Builder: public AgCompiler::Builder {
+	public:
+		virtual TrecPointer<AgCompiler> GetCompiler(TrecPointer<TFileShell> file, ag_lang_output output) override;
+	};
+
+	virtual TString SupportsFiles() override;
+
+	virtual TString SupportsOutput(TDataArray<ag_lang_output>& output) override;
+
+	virtual TString Configure(ag_lang_output output, TString& outputDetails, TrecPointer<TVariable> config) override;
+
+	virtual bool Prepare(TDataArray<CompileMessage>& messages, TrecSubPointer<TVariable, TContainerVariable>& details) override;
+
+	virtual int CompileResources(TDataArray<CompileMessage>& messages, TrecSubPointer<TVariable, TContainerVariable>& details) override;
+
+	virtual bool CompleteCompile(TDataArray<CompileMessage>& messages, TrecSubPointer<TVariable, TContainerVariable>& details) override;
+};
+
